97567645d8de5ddd1fd5e57967ff47503a4387eb
yomisana <sammywhy3236@gmail.com>
2024-06-29 15:33:13 +0800

    update self elevate install&uninstall script

--- install.ps1
+++ install.ps1
@@ -5,18 +5,29 @@ $serviceDisplayName = "VD Manager Service"
 # 服務描述
 $serviceDescription = "Service to manage Windows virtual desktops"
 # PowerShell 腳本的路徑
-$scriptPath = (Join-Path (Get-Location) "main.ps1")
+# 保存原始的工作目錄
+$originalPath = Get-Location
+$scriptPath = Join-Path $originalPath "main.ps1"
+Write-Output $($scriptPath)
+
+# 檢查是否以管理員權限運行
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-NoExit -c `"cd '$pwd'; & '" + $MyInvocation.MyCommand.Path + "'`""
+        Start-Process powershell -Verb runas -ArgumentList $CommandLine
+        Exit
+    }
+}
+
 
 # 檢查服務是否已存在
 if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
     Write-Host "服務 $serviceName 已經存在。"
+    Pause
     exit 1
 }
 
-# 創建服務
-New-Service -Name $serviceName -Binary "powershell.exe" -ArgumentList "-File `"$scriptPath`"" -DisplayName $serviceDisplayName -Description $serviceDescription -StartupType Automatic
 
-# 啟動服務
-Start-Service -Name $serviceName
+New-Service -Name $serviceName -BinaryPathName $scriptPath -DisplayName $serviceDisplayName -StartupType "Automatic" -Description $serviceDescription
 
-Write-Host "VDManagerService 已成功安裝並啟動。"
+Pause
--- uninstall.ps1
+++ uninstall.ps1
@@ -1,14 +1,26 @@
 # 服務名稱
 $serviceName = "VDManagerService"
 
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Warning "請以管理員權限運行此腳本！"
+    if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
+        Exit
+    }
+    Pause
+    #exit 1
+}
+
 # 停止服務
 if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
-    Stop-Service -Name $serviceName
+    #Stop-Service -Name $serviceName
     # 刪除服務
-    Remove-Service -Name $serviceName
-
-    Write-Host "VDManagerService 已成功卸載。"
+    #Remove-Service -Name $serviceName
+    sc.exe delete $serviceName
+    #Write-Host "VDManagerService 已成功卸載。"
 }
 else {
     Write-Host "服務 $serviceName 不存在。"
 }
+
+Pause
